17ccdde1bc3cc9cdcad56cd1b5b445e5b7d858c3
João Inácio <egnawake@gmail.com>
2024-01-14 17:51:08 +0000

    Add drag marker for stones

--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,11 +8,25 @@ public class DeckManager : MonoBehaviour
     [SerializeField] private Button mainMenuButton;
     [SerializeField] private CardGrid cardGrid;
     [SerializeField] private ElementalStone[] stones;
+    [SerializeField] private GameObject stoneDragMarkerPrefab;
+    [SerializeField] private Sprite fireIcon;
+    [SerializeField] private Sprite grassIcon;
+    [SerializeField] private Sprite waterIcon;
+
+    private PlayerResources pr;
+    private GameObject dragMarker;
 
     private void Start()
     {
+        pr = PlayerResources.Instance;
+
         mainMenuButton.onClick.AddListener(LoadMainMenu);
         cardGrid.OnStonePlaced += EnchantCard;
+        foreach (ElementalStone s in stones)
+        {
+            s.OnStonePickup += StonePickedUp;
+            s.OnStoneDrop += StoneDropped;
+        }
     }
 
     private void LoadMainMenu()
@@ -21,8 +36,6 @@ public class DeckManager : MonoBehaviour
 
     private void EnchantCard(Element element, int index)
     {
-        PlayerResources pr = PlayerResources.Instance;
-
         if (pr.GetStones(element) <= 0)
         {
             return;
@@ -37,6 +50,33 @@ public class DeckManager : MonoBehaviour
         foreach (ElementalStone s in stones)
         {
             s.UpdateAmount();
+        }
+    }
+
+    private void StonePickedUp(Element element)
+    {
+        if (pr.GetStones(element) <= 0)
+        {
+            return;
+        }
+
+        dragMarker = Instantiate(stoneDragMarkerPrefab, transform);
+        Image image = dragMarker.GetComponent<Image>();
+        image.sprite = element switch
+        {
+            Element.Fire => fireIcon,
+            Element.Grass => grassIcon,
+            Element.Water => waterIcon,
+            _ => throw new ArgumentException("Unknown element!")
+        };
+    }
+
+    private void StoneDropped()
+    {
+        if (dragMarker != null)
+        {
+            Destroy(dragMarker);
+            dragMarker = null;
         }
     }
 }
