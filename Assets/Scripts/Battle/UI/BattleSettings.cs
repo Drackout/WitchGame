918077dc555b56ff842354e9cb456a174fc1f89f
Drackout <mikatx2@hotmail.com>
2024-01-15 20:25:39 +0000

    Problems

--- Assets/Scripts/Battle/UI/BattleSettings.cs
+++ Assets/Scripts/Battle/UI/BattleSettings.cs
@@ -1,13 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleSettings : MonoBehaviour
 {
+    [SerializeField] private EncounterData[] encounters;
+
     private static BattleSettings instance;
+    private int encounterIndex;
 
     public static BattleSettings Instance => instance;
     public bool ShuffleDeck { get; set; }
+
+    public EncounterData CurrentEncounter => encounters[encounterIndex];
+
+    public EncounterData NextEncounter()
+    {
+        encounterIndex = Math.Min(encounters.Length - 1, encounterIndex + 1);
+        return CurrentEncounter;
+    }
 
     private void Awake()
     {
@@ -19,8 +31,9 @@ public class BattleSettings : MonoBehaviour
 
         instance = this;
 
-        ShuffleDeck = true;
-
         DontDestroyOnLoad(gameObject);
+
+        ShuffleDeck = true;
+        encounterIndex = 0;
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/EnemyCreature.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableObjects/CreateEnemy")]
+public class EnemyCreature : ScriptableObject
+{
+
+    public int health;
+    public int attackMin;
+    public int attackMax;
+    public Element element;
+    public string whatHits;
+    public string specialEffect;
+    public string[] drops;
+    public UICreature prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
